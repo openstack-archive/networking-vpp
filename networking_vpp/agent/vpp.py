7b9b59d48dbd733b842f871b3085a69ef6e8a55c
Sean <sechandl@cisco.com>
2016-09-23 11:04:32 -0400

    Add VPP VLAN tag rewrite to subinterfaces
    Ensure subinterfaces pop one vlan.  Set during creation and also
    pessimistically on reference.
    
    Change-Id: I48c48228dfa2d04fe9dfae2b38f7e654f48a2eae
    Signed-off-by: Sean <sechandl@cisco.com>

--- networking_vpp/agent/vpp.py
+++ networking_vpp/agent/vpp.py
@@ -19,6 +19,8 @@ import os
 import pwd
 import vpp_papi
 
+L2_VTR_POP_1 = 3
+
 
 def mac_to_bytes(mac):
     return str(''.join(chr(int(x, base=16)) for x in mac.split(':')))
@@ -168,6 +170,9 @@ class VPPInterface(object):
 
         self._check_retval(t)
 
+        # turn on VLAN tag pop for this subinterface (L2_VTR_POP_1=3)
+        self.set_vlan_tag_rewrite(t.sw_if_index)
+
         return t.sw_if_index
 
 #    def create_srcrep_vxlan_subif(self, vrf_id, src_addr, bcast_addr, vnid):
@@ -183,6 +188,18 @@ class VPPInterface(object):
 #
 #        return t.sw_if_index
     ########################################
+
+    def set_vlan_tag_rewrite(self, if_id, vtr_op=L2_VTR_POP_1, push_dot1q=0,
+                             tag1=0, tag2=0):
+        t = vpp_papi.l2_interface_vlan_tag_rewrite(
+            if_id,
+            vtr_op,
+            push_dot1q,
+            tag1,
+            tag2)
+        self.LOG.info("Set subinterface vlan tag pop response: %s" % str(t))
+
+        self._check_retval(t)
 
     def add_to_bridge(self, bridx, *ifidxes):
         for ifidx in ifidxes:
