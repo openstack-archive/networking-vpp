52ceb4a95c84d97ee8002571a7f59e8f9d1e19dc
Sean <sechandl@cisco.com>
2016-09-23 11:04:32 -0400

    Add VPP VLAN tag rewrite to subinterfaces
    Ensure subinterfaces pop one vlan.  Set during creation and also
    pessimistically on reference.
    
    Change-Id: I48c48228dfa2d04fe9dfae2b38f7e654f48a2eae
    Signed-off-by: Sean <sechandl@cisco.com>

--- networking_vpp/agent/server.py
+++ networking_vpp/agent/server.py
@@ -181,6 +181,7 @@ class VPPForwarder(object):
                                                          seg_id)
             else:
                 if_upstream = self.get_vpp_ifidx('%s.%s' % (intf, seg_id))
+                self.vpp.set_vlan_tag_rewrite(if_upstream)
         # elif net_type == 'vxlan':
         #     # NB physnet not really used here
         #     if_upstream = \
