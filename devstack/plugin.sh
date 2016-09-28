8eb9961602dd04039b5a0945dfd4462f31d193b3
Kevin Bringard <kevinbri@cisco.com>
2016-09-28 13:02:35 -0700

    Properly set etcd_port
    etcd_port was being set multiple times, as $ETCD_USER
    and as $ETCD_PASS. I believe this was in error, but more
    importantly, it was causing etcd_port to be set a blank
    value in the ml2_conf.ini file and leading to neutron failing
    to start due to a key error.
    
    This should resolve that.
    
    Change-Id: I68ea27009def89b2501885b1eb9942423e1d6013

--- devstack/plugin.sh
+++ devstack/plugin.sh
@@ -30,8 +30,8 @@ function configure_vpp {
     iniset /$Q_PLUGIN_CONF_FILE ml2_vpp physnets $MECH_VPP_PHYSNETLIST
     iniset /$Q_PLUGIN_CONF_FILE ml2_vpp etcd_host $ETCD_HOST
     iniset /$Q_PLUGIN_CONF_FILE ml2_vpp etcd_port $ETCD_PORT
-    iniset /$Q_PLUGIN_CONF_FILE ml2_vpp etcd_port $ETCD_USER
-    iniset /$Q_PLUGIN_CONF_FILE ml2_vpp etcd_port $ETCD_PASS
+    iniset /$Q_PLUGIN_CONF_FILE ml2_vpp etcd_user $ETCD_USER
+    iniset /$Q_PLUGIN_CONF_FILE ml2_vpp etcd_pass $ETCD_PASS
 
     if [ ! -z "$VXLAN_SRC_ADDR" ] ; then
        iniset /$Q_PLUGIN_CONF_FILE ml2_vpp vxlan_src_addr $VXLAN_SRC_ADDR
