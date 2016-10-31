3b7701321085fc94862841d3f57ab26502cf0120
Christophe Fontaine <christophe.fontaine@qosmos.com>
2016-10-31 14:03:14 +0100

    DO NOT MERGE - Rework of mech driver
    Rework of the exising mech driver to split features in subclasses.
    This will ease the integration of new features, such as TaaS.
    Same work for the agent.
    
    Change-Id: I1e66fa3550f850226b310f44680f89b3cd465ddf
    Signed-off-by: Christophe Fontaine <christophe.fontaine@qosmos.com>

--- networking_vpp/agent/server.py
+++ networking_vpp/agent/server.py
@@ -27,10 +27,13 @@
 # that work was not repeated here where the aim was to get the APIs
 # worked out.  The two codebases will merge in the future.
 
+from abc import ABCMeta
+from abc import abstractmethod
 import etcd
 import json
 import os
 import re
+import six
 import sys
 from threading import Thread
 import time
@@ -101,7 +104,7 @@ class VPPForwarder(object):
         self.interfaces = {}    # uuid: if idx
 
     def get_vpp_ifidx(self, if_name):
-        """Return VPP's interface index value for the network interface"""
+        """Return VPP's interface index value for the network interface."""
         if self.vpp.get_interface(if_name):
             return self.vpp.get_interface(if_name).sw_if_index
         else:
@@ -119,8 +122,7 @@ class VPPForwarder(object):
         return x
 
     def network_on_host(self, physnet, net_type, seg_id=None):
-        """Find or create a network of the type required"""
-
+        """Find or create a network of the type required."""
         if (physnet, net_type, seg_id) not in self.networks:
             self.create_network_on_host(physnet, net_type, seg_id)
         return self.networks.get((physnet, net_type, seg_id), None)
@@ -183,8 +185,12 @@ class VPPForwarder(object):
         if net is not None:
 
             self.vpp.delete_bridge_domain(net['bridge_domain_id'])
+            if net['network_type'] == 'vlan':
+                iface = self.vpp.get_interface(net['if_upstream'] +
+                                               '.' + str(seg_id))
+                self.vpp.delete_vlan_subif(iface.sw_if_index)
 
-            # We leave the interface up.  Other networks may be using it
+            self.networks.pop((physnet, net_type, seg_id))
         else:
             LOG.error("Delete Network: network is unknown "
                       "to agent")
@@ -234,7 +240,7 @@ class VPPForwarder(object):
     # TODO(njoy): make wait_time configurable
     # TODO(ijw): needs to be one thread for all waits
     def add_external_tap(self, device_name, bridge, bridge_name):
-        """Add an externally created TAP device to the bridge
+        """Add an externally created TAP device to the bridge.
 
         Wait for the external tap device to be created by the DHCP agent.
         When the tap device is ready, add it to bridge Run as a thread
@@ -329,6 +335,7 @@ class VPPForwarder(object):
         self.vpp.ifup(iface_idx)
         self.vpp.add_to_bridge(net_br_idx, iface_idx)
         props['net_data'] = net_data
+        props['net_data']['physnet'] = physnet
         LOG.debug('Bound vpp interface with sw_idx:%s on '
                   'bridge domain:%s'
                   % (iface_idx, net_br_idx))
@@ -376,18 +383,100 @@ class VPPForwarder(object):
             else:
                 LOG.error('Unknown port type %s during unbind'
                           % props['bind_type'])
+            self.interfaces.pop(uuid)
 
-        # TODO(ijw): delete structures of newly unused networks with
-        # delete_network
+            # Check if this is the last interface on host
+            for interface in self.interfaces.values():
+                if props['net_data'] == interface['net_data']:
+                    break
+            else:
+                # Network is not used on this host, delete it
+                net = props['net_data']
+                self.delete_network_on_host(net['physnet'],
+                                            net['network_type'],
+                                            net['segmentation_id'])
 
 
 ######################################################################
+
+
+@six.add_metaclass(ABCMeta)
+class AgentFeature(object):
+    path = ''
+
+    def __init__(self, host, etcd_client, vppf):
+        self.host = host
+        self.etcd_client = etcd_client
+        self.etcd_helper = EtcdHelper(self.etcd_client)
+        self.vppf = vppf
+        self.node_key_space = LEADIN + '/nodes/%s/%s' % (host, self.path)
+        self.state_key_space = LEADIN + '/state/%s/%s' % (host, self.path)
+        pass
+
+    @abstractmethod
+    def create(self, key, value):
+        pass
+
+    @abstractmethod
+    def set(self, key, value):
+        pass
+
+    @abstractmethod
+    def delete(self, key, value):
+        pass
+
+    @abstractmethod
+    def resync(self):
+        pass
+
+
+class PortAgentFeature(AgentFeature):
+    path = 'ports'
+
+    def __init__(self, host, etcd_client, vppf):
+        super(PortAgentFeature, self).__init__(host, etcd_client, vppf)
+        self.etcd_helper.clear_state(self.state_key_space)
+
+    def create(self, key, value):
+        # Create or update == bind
+        port = key
+        data = json.loads(value)
+        props = self.vppf.bind_interface_on_host(data['binding_type'],
+                                                 port,
+                                                 data['mac_address'],
+                                                 data['physnet'],
+                                                 data['network_type'],
+                                                 data['segmentation_id'])
+        self.etcd_client.write(self.state_key_space +
+                               '/%s' % port,
+                               json.dumps(props))
+
+    def set(self, key, value):
+        # Create or update == bind
+        self.create(key, value)
+
+    def delete(self, key, value):
+        # Removing key == desire to unbind
+        port = key
+        self.vppf.unbind_interface_on_host(port)
+        try:
+            self.etcd_client.delete(self.state_key_space +
+                                    '/%s' % port)
+        except etcd.EtcdKeyNotFound:
+            # Gone is fine, if we didn't delete it
+            # it's no problem
+            pass
+
+    def resync(self):
+        pass
+
 
 LEADIN = '/networking-vpp'  # TODO(ijw): make configurable?
 
 
 class EtcdListener(object):
     def __init__(self, host, etcd_client, vppf, physnets):
+        self.features = {}
         self.host = host
         self.etcd_client = etcd_client
         self.vppf = vppf
@@ -407,25 +496,10 @@ class EtcdListener(object):
     def repop_interfaces(self):
         pass
 
-    # The vppf bits
-
-    def unbind(self, id):
-        self.vppf.unbind_interface_on_host(id)
-
-    def bind(self, id, binding_type, mac_address, physnet, network_type,
-             segmentation_id):
-        # args['binding_type'] in ('vhostuser', 'plugtap'):
-        return self.vppf.bind_interface_on_host(binding_type,
-                                                id,
-                                                mac_address,
-                                                physnet,
-                                                network_type,
-                                                segmentation_id)
-
     HEARTBEAT = 60  # seconds
 
     def _sync_state(self, port_key_space):
-        """Sync VPP port state with etcd and return correct watchIndex"""
+        """Sync VPP port state with etcd and return correct watchIndex."""
         LOG.debug('Syncing VPP port state with etcd..')
         rv = self.etcd_client.read(port_key_space, recursive=True)
         for child in rv.children:
@@ -446,6 +520,12 @@ class EtcdListener(object):
                                        json.dumps(props))
         return rv.etcd_index
 
+    def register_feature(self, feature):
+        if feature.path in self.features.keys():
+            raise Exception('Feature "%s" already registered' % feature.path)
+        self.features[feature.path] = feature
+        LOG.debug('Added Feature: %s' % feature.path)
+
     def process_ops(self):
         # TODO(ijw): needs to remember its last tick on reboot, or
         # reconfigure from start (which means that VPP needs it
@@ -455,89 +535,70 @@ class EtcdListener(object):
             self.etcd_client.write(LEADIN + '/state/%s/physnets/%s'
                                    % (self.host, f), 1)
 
-        port_key_space = LEADIN + "/nodes/%s/ports" % self.host
-        state_key_space = LEADIN + "/state/%s/ports" % self.host
-        self.etcd_helper.clear_state(state_key_space)
-        tick = self._sync_state(port_key_space)
+        watch_key_space = LEADIN + "/nodes/%s" % self.host
+
+        tick = None
         LOG.debug("Starting watch on ports key space from Index: %s" % tick)
         while True:
-
-            # The key that indicates to people that we're alive
-            # (not that they care)
-            self.etcd_client.write(LEADIN + '/state/%s/alive' % self.host,
-                                   1, ttl=3 * self.HEARTBEAT)
-
             try:
-                LOG.error("ML2_VPP(%s): thread pausing"
-                          % self.__class__.__name__)
-                rv = self.etcd_client.watch(port_key_space,
-                                            recursive=True,
-                                            index=tick,
-                                            timeout=self.HEARTBEAT)
-                LOG.error('watch received %s on %s at tick %s',
-                          rv.action, rv.key, rv.modifiedIndex)
-                # TODO(ijw) can this be etcd_index?
-                # Now we have resynced, we know that all data is recovered
-                # not just to the last modified index, but all the way
-                # to the present (or we would have had a newer last
-                # modified index)
-                tick = rv.modifiedIndex + 1
-                LOG.error("ML2_VPP(%s): thread active"
-                          % self.__class__.__name__)
+                # The key that indicates to people that we're alive
+                # (not that they care)
+                self.etcd_client.write(LEADIN + '/state/%s/alive' % self.host,
+                                       1, ttl=3 * self.HEARTBEAT)
 
-                # Matches a port key, gets host and uuid
-                m = re.match(port_key_space + '/([^/]+)$',
-                             rv.key)
+                try:
+                    if tick is None:
+                        raise etcd.EtcdEventIndexCleared()
 
-                if m:
-                    port = m.group(1)
+                    LOG.debug("ML2_VPP(%s): thread pausing"
+                              % self.__class__.__name__)
 
-                    if rv.action == 'delete':
-                        # Removing key == desire to unbind
-                        self.unbind(port)
-                        try:
-                            self.etcd_client.delete(
-                                state_key_space + '/%s'
-                                % port)
-                        except etcd.EtcdKeyNotFound:
-                            # Gone is fine, if we didn't delete it
-                            # it's no problem
-                            pass
+                    rv = self.etcd_client.watch(watch_key_space,
+                                                recursive=True,
+                                                index=tick,
+                                                timeout=self.HEARTBEAT)
+                    vals = [rv]
+                    LOG.debug('watch received %s on %s at tick %s',
+                              rv.action, rv.key, rv.modifiedIndex)
+                    # TODO(ijw) can this be etcd_index?
+                    # Now we have resynced, we know that all data is recovered
+                    # not just to the last modified index, but all the way
+                    # to the present (or we would have had a newer last
+                    # modified index)
+                    tick = rv.modifiedIndex + 1
+
+                except etcd.EtcdEventIndexCleared:
+                    rv = self.etcd_client.read(watch_key_space,
+                                               recursive=True)
+                    vals = rv.children
+                    for feature in self.features.values():
+                        feature.resync()
+                    tick = rv.etcd_index + 1
+
+                for kv in vals:
+                    LOG.error("ML2_VPP(%s): thread active"
+                              % self.__class__.__name__)
+
+                    m = re.match(LEADIN + '/nodes/' + self.host +
+                                 '/([^/]+)/([^/]+)$', kv.key)
+
+                    if m:
+                        feature = m.group(1)
+                        key = m.group(2)
+                        if kv.action is None:
+                            # occurs when resyncing
+                            kv.action = 'create'
+                        method = kv.action
+                        feature_instance = self.features[feature]
+                        getattr(feature_instance, method)(key, kv.value)
+
                     else:
-                        # Create or update == bind
-                        data = json.loads(rv.value)
-                        props = self.bind(port,
-                                          data['binding_type'],
-                                          data['mac_address'],
-                                          data['physnet'],
-                                          data['network_type'],
-                                          data['segmentation_id'])
-                        self.etcd_client.write(state_key_space + '/%s'
-                                               % port,
-                                               json.dumps(props))
-
-                else:
-                    LOG.warn('Unexpected key change in etcd port feedback, '
-                             'key %s' % rv.key)
+                        LOG.warn('Unexpected key change in etcd port feedback,'
+                                 'key %s' % kv.key)
 
             except (etcd.EtcdWatchTimedOut, TimeoutError):
                 # This is normal
                 pass
-            except etcd.EtcdEventIndexCleared:
-                LOG.debug("Received etcd event index cleared. "
-                          "Recovering etcd watch index")
-                rv = self.etcd_client.read(port_key_space)
-                # TODO(ijw): we need to resync our state
-                # with the value of 'rv' here
-                # Now we have resynced, we know that all data is recovered
-                # not just to the last modified index, but all the way
-                # to the present (or we would have had a newer last
-                # modified index).
-                # A resync need not involve writing the physnets (they don't
-                # change) but should involve updating any ports in VPP that are
-                # not in the state we believe they should be in.
-                tick = rv.etcd_index + 1
-                LOG.debug("Etcd watch index recovered at %s" % tick)
             except Exception as e:
                 LOG.error('etcd threw exception %s' % traceback.format_exc(e))
 
@@ -562,14 +623,13 @@ class VPPRestart(object):
 def main():
     cfg.CONF(sys.argv[1:])
     logging.setup(cfg.CONF, 'vpp_agent')
+    LOG.debug('Restarting VPP..')
+    VPPRestart().wait()
 
     # If the user and/or group are specified in config file, we will use
     # them as configured; otherwise we try to use defaults depending on
     # distribution. Currently only supporting ubuntu and redhat.
     cfg.CONF.register_opts(config_opts.vpp_opts, "ml2_vpp")
-    if cfg.CONF.ml2_vpp.enable_vpp_restart:
-        LOG.debug('Restarting VPP..')
-        VPPRestart().wait()
 
     physnet_list = cfg.CONF.ml2_vpp.physnets.replace(' ', '').split(',')
     physnets = {}
@@ -602,6 +662,11 @@ def main():
                               allow_reconnect=True)
 
     ops = EtcdListener(cfg.CONF.host, etcd_client, vppf, physnets)
+    enabled_features = [PortAgentFeature]
+    # TODO(cfontaine): Read enabled features from configuration file
+    #       Only the 'PortAgentFeature' is mandatory
+    for feature in enabled_features:
+        ops.register_feature(feature(cfg.CONF.host, etcd_client, vppf))
 
     ops.process_ops()
 
