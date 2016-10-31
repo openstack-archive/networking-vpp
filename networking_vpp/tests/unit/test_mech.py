13bb630bdbb67f75ea0915c22b229f692f8fd753
Christophe Fontaine <christophe.fontaine@qosmos.com>
2016-10-31 14:03:14 +0100

    Rework of mech driver and agent
    Rework of the exising mech driver to split features in subclasses.
    This will ease the integration of new features, such as TaaS or
    L3 router.
    Same work for the agent.
    
    Agent Features use stevedore for extensions, using
    the namespace 'networking_vpp.agent.features'.
    
    Change-Id: I1e66fa3550f850226b310f44680f89b3cd465ddf
    Signed-off-by: Christophe Fontaine <christophe.fontaine@qosmos.com>

--- networking_vpp/tests/unit/test_mech.py
+++ networking_vpp/tests/unit/test_mech.py
@@ -159,13 +159,13 @@ class VPPMechanismDriverTestCase(base.BaseTestCase):
             "Return value for port [%s] should have been False" % (
                 port_context.current.id)
 
-    @mock.patch('networking_vpp.mech_vpp.EtcdAgentCommunicator.unbind')
-    @mock.patch('networking_vpp.mech_vpp.EtcdAgentCommunicator.bind')
+    @mock.patch('networking_vpp.mech_vpp.FeaturePortBinding.unbind')
+    @mock.patch('networking_vpp.mech_vpp.FeaturePortBinding.bind')
     def test_update_port_precommit(self, m_bind, m_unbind):
         port_context = self.given_port_context()
         current_bind = port_context.binding_levels[-1]
         self.mech.update_port_precommit(port_context)
-        self.mech.communicator.bind.assert_called_once_with(
+        self.mech.ports.bind.assert_called_once_with(
             port_context._plugin_context.session,
             port_context.current,
             current_bind[api.BOUND_SEGMENT],
@@ -173,7 +173,7 @@ class VPPMechanismDriverTestCase(base.BaseTestCase):
             'vhostuser')
         port_context.binding_levels = [None]
         self.mech.update_port_precommit(port_context)
-        self.mech.communicator.unbind.assert_called_once_with(
+        self.mech.ports.unbind.assert_called_once_with(
             port_context._plugin_context.session,
             port_context.current,
             port_context.original_host)
@@ -187,11 +187,11 @@ class VPPMechanismDriverTestCase(base.BaseTestCase):
         self.mech.update_port_postcommit(port_context)
         self.mech.communicator.kick.assert_called()
 
-    @mock.patch('networking_vpp.mech_vpp.EtcdAgentCommunicator.unbind')
+    @mock.patch('networking_vpp.mech_vpp.FeaturePortBinding.unbind')
     def test_delete_port_precommit(self, m_unbind):
         port_context = self.given_port_context()
         self.mech.delete_port_precommit(port_context)
-        self.mech.communicator.unbind.called_once_with(
+        self.mech.ports.unbind.called_once_with(
             port_context._plugin_context.session,
             port_context.current,
             port_context.host)
