c8802a14be046547767cba8300e7082798e6eea7
Ian Wells <iawells@cisco.com>
2016-11-15 11:30:22 -0800

    Liberty compatibility
    For those that need it, this will work with Liberty but in one or two
    cases uses more modern parts of the Neutron interface.
    
    This patches in the additional functions to the Neutron interface to
    make that possible.  It's not terribly sanitary, but the compat layer
    is designed not to make changes where they aren't called for.
    
    Change-Id: I33e53c7f3e0c70f2dda9f06a3ce469bad8c8416c

---
+++ networking_vpp/utils/compat.py
@@ -0,0 +1,33 @@
+# Copyright (c) 2016 Cisco Systems, Inc.
+# All Rights Reserved.
+#
+#    Licensed under the Apache License, Version 2.0 (the "License"); you may
+#    not use this file except in compliance with the License. You may obtain
+#    a copy of the License at
+#
+#         http://www.apache.org/licenses/LICENSE-2.0
+#
+#    Unless required by applicable law or agreed to in writing, software
+#    distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+#    WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+#    License for the specific language governing permissions and limitations
+#    under the License.
+
+import os
+
+from neutron.agent.linux import bridge_lib
+
+# Loading this library monkey-patches a couple of bits of Neutron
+# to enable compatibility with Liberty
+
+# Liberty does not include the 'owns_interface' function
+if 'owns_interface' not in dir(bridge_lib.BridgeDevice):
+
+    BRIDGE_INTERFACE_FS = bridge_lib.BRIDGE_FS + "%(bridge)s/brif/%(interface)s"
+
+    def owns_interface(self, interface):
+        return os.path.exists(
+            BRIDGE_INTERFACE_FS % {'bridge': self.name,
+                                   'interface': interface})
+
+    bridge_lib.owns_interface = owns_interface
