010515272505a99cdfcb3ee3719b34332ca4db6b
Shriram Chander <shrircha@cisco.com>
2016-11-28 09:43:42 -0800

    tests/unit/*: Enhancing unit tests for ML2
    Adds tests for the following classes:
    * EtcdAgentCommunicator
    * VPPForwarder
    
    Change-Id: I22772f63d2c3e4614e47862ef124b15c22804ca7

---
+++ networking_vpp/tests/unit/test_vppforwarder.py
@@ -0,0 +1,195 @@
+# Copyright (c) 2016 Cisco Systems, Inc.
+# All Rights Reserved.
+#
+#    Licensed under the Apache License, Version 2.0 (the "License"); you may
+#    not use this file except in compliance with the License. You may obtain
+#    a copy of the License at
+#
+#         http://www.apache.org/licenses/LICENSE-2.0
+#
+#    Unless required by applicable law or agreed to in writing, software
+#    distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+#    WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+#    License for the specific language governing permissions and limitations
+#    under the License.
+
+import mock
+import sys
+sys.modules['vpp_papi'] = mock.MagicMock()
+from networking_vpp.agent import server
+from neutron.tests import base
+
+
+class VPPForwarderTestCase(base.BaseTestCase):
+    _mechanism_drivers = ['vpp']
+
+    @mock.patch('networking_vpp.agent.server.vpp.VPPInterface.get_interface')
+    @mock.patch('networking_vpp.agent.server.vpp')
+    def setUp(self, m_vpp, m_vppif):
+        super(VPPForwarderTestCase, self).setUp()
+        self.vpp = server.VPPForwarder({"test_net": "test_iface"})
+
+    def test_get_vpp_ifidx(self):
+        type(self.vpp.vpp.get_interface.return_value).sw_if_index = 1
+        sw_if_return = self.vpp.get_vpp_ifidx('test')
+        self.vpp.vpp.get_interface.assert_called_with('test')
+        assert (sw_if_return == 1), "Return value should have been 1"
+
+    def test_get_interface(self):
+        retval = self.vpp.get_interface('test_net')
+        assert (retval == 'test_iface'), \
+            "Return value should have been test_iface"
+
+    def test_new_bridge_domain(self):
+        bridge_id = 5678
+        self.vpp.new_bridge_domain()
+        self.vpp.vpp.create_bridge_domain.assert_called_once_with(bridge_id)
+        assert (self.vpp.next_bridge_id == 5679),\
+            "Bridge ID should now be 5679"
+
+    @mock.patch(
+        'networking_vpp.agent.server.VPPForwarder.create_network_on_host')
+    def test_no_network_on_host(self, m_create_network_on_host):
+        physnet = 'test'
+        self.vpp.network_on_host(physnet, 'flat')
+        assert m_create_network_on_host.called_once_with(physnet, 'flat', None)
+
+    @mock.patch(
+        'networking_vpp.agent.server.VPPForwarder.create_network_on_host')
+    def test_yes_network_on_host(self, m_create_network_on_host):
+        physnet = 'test'
+        self.vpp.networks = {(physnet, 'flat', None): 'test'}
+        retval = self.vpp.network_on_host(physnet, 'flat')
+        assert(retval == 'test'), "Return network value should be 'test'"
+
+    def test_none_create_network_on_host(self):
+        retval = self.vpp.create_network_on_host('not_there', 'flat', None)
+        assert (retval is None), "Return value should have been None"
+
+    def test_flat_create_network_on_host(self):
+        net_length = len(self.vpp.networks)
+        type(self.vpp.vpp.get_interface.return_value).sw_if_index = 1
+        self.vpp.create_network_on_host('test_net', 'flat', '1')
+        self.vpp.vpp.ifup.assert_called_once_with(1)
+        self.vpp.vpp.add_to_bridge.called_once_with(5679, 1)
+        assert (len(self.vpp.networks) > net_length), \
+            "There should be one more network now"
+
+    def test_vlan_create_network_on_host(self):
+        net_length = len(self.vpp.networks)
+        type(self.vpp.vpp.get_interface.return_value).sw_if_index = 1
+        self.vpp.vpp.get_interface('test_iface.1').return_value = 1
+        self.vpp.create_network_on_host('test_net', 'vlan', '1')
+        self.vpp.vpp.ifup.assert_called_with(1)
+        self.vpp.vpp.add_to_bridge.assert_called_once_with(5678, 1)
+        assert (len(self.vpp.networks) > net_length), \
+            "There should be one more network now"
+
+    def test_delete_network_on_host(self):
+        physnet = 'test'
+        self.vpp.networks = {(physnet, 'flat', None): {'bridge_domain_id': 1}}
+        self.vpp.delete_network_on_host(physnet, 'flat')
+        self.vpp.vpp.delete_bridge_domain.assert_called_once_with(1)
+
+    @mock.patch('networking_vpp.agent.server.ip_lib')
+    def test_bridge_exists_and_ensure_up(self, m_ip_lib):
+        retval = self.vpp._bridge_exists_and_ensure_up('test')
+        assert (retval is True), "Bridge link should have been found"
+
+    @mock.patch('networking_vpp.agent.server.bridge_lib')
+    def test_ensure_bridge_found(self, m_br_lib):
+        self.vpp.ensure_bridge('test_br')
+        assert m_br_lib.BridgeDevice.called_once_with('test_br')
+
+    @mock.patch('networking_vpp.agent.server.bridge_lib')
+    @mock.patch(
+        'networking_vpp.agent.server.VPPForwarder._bridge_exists_and_ensure_up'
+        )
+    def test_ensure_bridge(self, m_br_ex, m_br_lib):
+        m_br_ex.return_value = False
+        m_br_lib.BridgeDevice.setfd.return_value = False
+        self.vpp.ensure_bridge('test_br')
+        assert m_br_lib.BridgeDevice.addbr.called_once_with('test_br')
+
+    @mock.patch('networking_vpp.agent.server.bridge_lib')
+    @mock.patch('networking_vpp.agent.server.ip_lib')
+    def test_add_external_tap(self, m_ip_lib, m_br_lib):
+        m_ip_lib.device_exists.return_value = True
+        device_name = "fake_dev"
+        bridge = mock.MagicMock()
+        bridge.owns_interface.return_value = False
+        bridge_name = "fake_br"
+        self.vpp.add_external_tap(device_name, bridge, bridge_name)
+        bridge.addif.assert_called_once_with(device_name)
+
+    def test_create_interface_on_host_exists(self):
+        if_type = 'maketap'
+        uuid = 'fakeuuid'
+        mac = 'fakemac'
+        fake_iface = {'bind_type': if_type,
+                      'iface_idx': 1,
+                      'mac': mac}
+        self.vpp.interfaces = {uuid: fake_iface}
+        retval = self.vpp.create_interface_on_host(if_type, uuid, mac)
+        assert (retval == fake_iface)
+
+    def test_create_interface_on_host_maketap(self):
+        if_type = 'maketap'
+        uuid = 'fakeuuid'
+        mac = 'fakemac'
+        retval = self.vpp.create_interface_on_host(if_type, uuid, mac)
+        self.vpp.vpp.create_tap.assert_called_once_with('tapfakeuuid', mac)
+        assert (retval == self.vpp.interfaces[uuid])
+
+    @mock.patch('networking_vpp.agent.server.VPPForwarder.ensure_bridge')
+    @mock.patch('networking_vpp.agent.server.Thread')
+    def test_create_interface_on_host_plugtap(self, m_thread, m_en_br):
+        if_type = 'plugtap'
+        uuid = 'fakeuuid'
+        mac = 'fakemac'
+        retval = self.vpp.create_interface_on_host(if_type, uuid, mac)
+        self.vpp.vpp.create_tap.assert_called_once_with('vppfakeuuid', mac)
+        self.vpp.ensure_bridge.assert_called_once_with('br-fakeuuid')
+        assert (retval == self.vpp.interfaces[uuid])
+
+    def test_create_interface_on_host_vhostuser(self):
+        if_type = 'vhostuser'
+        uuid = 'fakeuuid'
+        mac = 'fakemac'
+        retval = self.vpp.create_interface_on_host(if_type, uuid, mac)
+        self.vpp.vpp.create_vhostuser.assert_called_once_with('/tmp/fakeuuid',
+                                                              mac, None, None)
+        assert (retval == self.vpp.interfaces[uuid])
+
+    #(schander) re-enable this test once there's a more specific Exception
+    # in code
+    #def test_create_interface_on_host_unsupported(self):
+    #    if_type = 'unsupported'
+    #    uuid = 'fakeuuid'
+    #    mac = 'fakemac'
+    #    self.assertRaises(Exception,
+    #                      self.vpp.create_interface_on_host,
+    #                      if_type, uuid, mac)
+
+    @mock.patch('networking_vpp.agent.server.VPPForwarder.network_on_host')
+    @mock.patch(
+        'networking_vpp.agent.server.VPPForwarder.create_interface_on_host')
+    def test_bind_interface_on_host(self, m_create_iface_on_host,
+                                    m_network_on_host):
+        if_type = 'plugtap'
+        uuid = 'fakeuuid'
+        mac = 'fakemac'
+        physnet = 'fakenet'
+        net_type = 'flat'
+        seg_id = 1
+        m_network_on_host.return_value = {'bridge_domain_id': 'fake_dom_id'}
+        m_create_iface_on_host.return_value = {'iface_idx': 'fakeidx'}
+        expected_val = {'iface_idx': 'fakeidx',
+                        'net_data': {'bridge_domain_id': 'fake_dom_id'}}
+        retval = self.vpp.bind_interface_on_host(if_type,
+                                                 uuid, mac, physnet,
+                                                 net_type, seg_id)
+        assert (retval == expected_val)
+
+    def test_unbind_interface_on_host(self):
+        pass
