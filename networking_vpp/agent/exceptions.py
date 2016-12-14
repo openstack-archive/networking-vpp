3a16225c093fd46a98c2514c9fa447beaa67a0c7
Ian Wells <iawells@cisco.com>
2016-12-13 16:07:29 -0800

    Fix translator functions.
    Should use the local one, not the Neutron one (which has changed
    name from Liberty to Mitaka anyway, making it a compatibility
    problem).
    
    Change-Id: Iec6f21063734dcb3bbad7f7c610015552646fc3d

--- networking_vpp/agent/exceptions.py
+++ networking_vpp/agent/exceptions.py
@@ -13,7 +13,7 @@
 #    License for the specific language governing permissions and limitations
 #    under the License.
 
-from neutron._i18n import _
+from networking_vpp._i18n import _
 from neutron_lib import exceptions as n_exec
 
 
