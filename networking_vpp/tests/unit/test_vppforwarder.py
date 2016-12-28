bbda4c5497e4aa56c4c426ecfb4f4147ece97d8f
Christophe Fontaine <christophe.fontaine@qosmos.com>
2016-12-28 17:27:18 +0100

    Improvements on agent restart
    Adding the port uuid of the interface as the 'tag' for vpp,
    we are able not to rebind interfaces to qemu when the agent
    is restarted.
    
    The resync is done in 3 phases:
    - fill the agent cache (resync_start)
    - read and apply configuration from etcd
    - cleanup vpp (resync_end)
    
    Change-Id: I655a1532164c80a91ccb6affe15b961751ffa14d
    Signed-off-by: Christophe Fontaine <christophe.fontaine@qosmos.com>

--- networking_vpp/tests/unit/test_vppforwarder.py
+++ networking_vpp/tests/unit/test_vppforwarder.py
@@ -24,29 +24,29 @@ from neutron.tests import base
 class VPPForwarderTestCase(base.BaseTestCase):
     _mechanism_drivers = ['vpp']
 
-    @mock.patch('networking_vpp.agent.server.vpp.VPPInterface.get_interface')
+    @mock.patch('networking_vpp.agent.server.vpp.VPPInterface.'
+                'get_ifidx_by_name')
+    @mock.patch('networking_vpp.agent.server.vpp.VPPInterface.'
+                'get_ifidx_by_tag')
     @mock.patch('networking_vpp.agent.server.vpp')
-    def setUp(self, m_vpp, m_vppif):
+    def setUp(self, m_vpp, m_vppifname, m_vppiftag):
         super(VPPForwarderTestCase, self).setUp()
         self.vpp = server.VPPForwarder({"test_net": "test_iface"})
 
-    def test_get_vpp_ifidx(self):
-        type(self.vpp.vpp.get_interface.return_value).sw_if_index = 1
-        sw_if_return = self.vpp.get_vpp_ifidx('test')
-        self.vpp.vpp.get_interface.assert_called_with('test')
-        assert (sw_if_return == 1), "Return value should have been 1"
+        def idxes(iface):
+            vals = {
+                'test_iface': 720,
+                'test_iface.1': 740
+            }
+            return vals.get(iface, None)
+        self.vpp.vpp.get_ifidx_by_name.side_effect = idxes
+        self.vpp.vpp.get_ifidx_by_tag.return_value = None
 
-    def test_get_interface(self):
-        retval = self.vpp.get_interface('test_net')
-        assert (retval == 'test_iface'), \
-            "Return value should have been test_iface"
-
-    def test_new_bridge_domain(self):
-        bridge_id = 5678
-        self.vpp.new_bridge_domain()
-        self.vpp.vpp.create_bridge_domain.assert_called_once_with(bridge_id)
-        assert (self.vpp.next_bridge_id == 5679),\
-            "Bridge ID should now be 5679"
+    def test_get_if_for_physnet(self):
+        (ifname, ifidx) = self.vpp.get_if_for_physnet('test_net')
+        self.vpp.vpp.get_ifidx_by_name.assert_called_once_with('test_iface')
+        assert (ifname == 'test_iface'), 'test_net is on test_iface'
+        assert (ifidx == 720), 'test_iface has idx 720'
 
     @mock.patch(
         'networking_vpp.agent.server.VPPForwarder.create_network_on_host')
@@ -69,21 +69,18 @@ class VPPForwarderTestCase(base.BaseTestCase):
 
     def test_flat_create_network_on_host(self):
         net_length = len(self.vpp.networks)
-        type(self.vpp.vpp.get_interface.return_value).sw_if_index = 1
         self.vpp.create_network_on_host('test_net', 'flat', '1')
-        self.vpp.vpp.ifup.assert_called_once_with(1)
-        self.vpp.vpp.add_to_bridge.called_once_with(5679, 1)
-        assert (len(self.vpp.networks) > net_length), \
+        self.vpp.vpp.ifup.assert_called_once_with(720)
+        self.vpp.vpp.add_to_bridge.called_once_with(5679, 720)
+        assert (len(self.vpp.networks) == 1 + net_length), \
             "There should be one more network now"
 
     def test_vlan_create_network_on_host(self):
         net_length = len(self.vpp.networks)
-        type(self.vpp.vpp.get_interface.return_value).sw_if_index = 1
-        self.vpp.vpp.get_interface('test_iface.1').return_value = 1
         self.vpp.create_network_on_host('test_net', 'vlan', '1')
-        self.vpp.vpp.ifup.assert_called_with(1)
-        self.vpp.vpp.add_to_bridge.assert_called_once_with(5678, 1)
-        assert (len(self.vpp.networks) > net_length), \
+        self.vpp.vpp.ifup.assert_called_with(740)
+        self.vpp.vpp.add_to_bridge.assert_called_once_with(740, 740)
+        assert (len(self.vpp.networks) == 1 + net_length), \
             "There should be one more network now"
 
     def test_delete_network_on_host(self):
@@ -131,7 +128,7 @@ class VPPForwarderTestCase(base.BaseTestCase):
     def test_create_interface_on_host_exists(self):
         if_type = 'maketap'
         uuid = 'fakeuuid'
-        mac = 'fakemac'
+        mac = '00:00:00:00:00:00'
         fake_iface = {'bind_type': if_type,
                       'iface_idx': 1,
                       'mac': mac}
@@ -142,34 +139,39 @@ class VPPForwarderTestCase(base.BaseTestCase):
     def test_create_interface_on_host_maketap(self):
         if_type = 'maketap'
         uuid = 'fakeuuid'
-        mac = 'fakemac'
+        mac = '00:00:00:00:00:00'
         retval = self.vpp.create_interface_on_host(if_type, uuid, mac)
-        self.vpp.vpp.create_tap.assert_called_once_with('tapfakeuuid', mac)
+        self.vpp.vpp.get_ifidx_by_tag.assert_called_once_with(uuid)
+        self.vpp.vpp.create_tap.assert_called_once_with('tapfakeuuid',
+                                                        mac, uuid)
         assert (retval == self.vpp.interfaces[uuid])
 
     @mock.patch('networking_vpp.agent.server.VPPForwarder.ensure_bridge')
     def test_create_interface_on_host_plugtap(self, m_en_br):
         if_type = 'plugtap'
         uuid = 'fakeuuid'
-        mac = 'fakemac'
+        mac = '00:00:00:00:00:00'
         retval = self.vpp.create_interface_on_host(if_type, uuid, mac)
-        self.vpp.vpp.create_tap.assert_called_once_with('vppfakeuuid', mac)
+        self.vpp.vpp.get_ifidx_by_tag.assert_called_once_with(uuid)
+        self.vpp.vpp.create_tap.assert_called_once_with('vppfakeuuid',
+                                                        mac, uuid)
         self.vpp.ensure_bridge.assert_called_once_with('br-fakeuuid')
         assert (retval == self.vpp.interfaces[uuid])
 
     def test_create_interface_on_host_vhostuser(self):
         if_type = 'vhostuser'
         uuid = 'fakeuuid'
-        mac = 'fakemac'
+        mac = '00:00:00:00:00:00'
         retval = self.vpp.create_interface_on_host(if_type, uuid, mac)
+        self.vpp.vpp.get_ifidx_by_tag.assert_called_once_with(uuid)
         self.vpp.vpp.create_vhostuser.assert_called_once_with('/tmp/fakeuuid',
-                                                              mac)
+                                                              mac, uuid)
         assert (retval == self.vpp.interfaces[uuid])
 
     def test_create_interface_on_host_unsupported(self):
         if_type = 'unsupported'
         uuid = 'fakeuuid'
-        mac = 'fakemac'
+        mac = '00:00:00:00:00:00'
         self.assertRaises(server.UnsupportedInterfaceException,
                           self.vpp.create_interface_on_host,
                           if_type, uuid, mac)
