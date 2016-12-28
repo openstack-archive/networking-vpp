523eb3763002af95db1461f28521fd1952de1102
Anne McCormick <amccormi@cisco.com>
2016-12-27 20:17:00 -0500

    Adding support for neutron alembic db migration to Liberty for networking_vpp plugin.
    In Liberty, alembic migration uses a HEADS file containing both the expand and
    contract heads, as opposed to splitting them out into different files, which is
    the current way of doing it. Also, the mitaka expand/contract scripts needed to
    be copied over for Liberty.
    
    Change-Id: Ibbd63ee1a7e0832b8a8d2a99d43ce9cf9dcb46fc
    closes-bug: 1652859

---
+++ networking_vpp/db/migration/alembic_migrations/versions/liberty/contract/12345678747070_initial.py
@@ -0,0 +1,31 @@
+#    Licensed under the Apache License, Version 2.0 (the "License"); you may
+#    not use this file except in compliance with the License. You may obtain
+#    a copy of the License at
+#
+#         http://www.apache.org/licenses/LICENSE-2.0
+#
+#    Unless required by applicable law or agreed to in writing, software
+#    distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+#    WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+#    License for the specific language governing permissions and limitations
+#    under the License.
+#
+
+"""start networking-vpp contract branch
+
+Revision ID: 12345678747070
+Create Date: 2016-08-04 12:34:56.000000
+
+"""
+
+from neutron.db.migration import cli
+
+
+# revision identifiers, used by Alembic.
+revision = '12345678747070'
+down_revision = '00656e76747070'
+branch_labels = (cli.CONTRACT_BRANCH,)
+
+
+def upgrade():
+    pass
