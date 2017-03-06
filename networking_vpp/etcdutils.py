8d4dc4eab8bb9680c572b49c67a0360b416d760e
Ian Wells <iawells@cisco.com>
2017-03-07 07:15:02 +1100

    (WIP) change EtcdChangeWatcher to have a nicer API
    Change-Id: Ifc1c6dfa6732a92ded838d5fd48f2fafce64d4d1

--- networking_vpp/etcdutils.py
+++ networking_vpp/etcdutils.py
@@ -389,7 +389,7 @@ class EtcdChangeWatcher(EtcdWatcher):
 
     This deals with the start/resync/watch dilemmas and makes a single
     'this key has changed' call regardless of what prompts it.
-    It does this by remembering what it's notified (via key_changed) in
+    It does this by remembering what it's notified (via key_change) in
     the past, and avoiding any notification that amounts to 'this key has
     changed to the same value'.  However, when we have to do a full resync
     the ordering of key updates is not guaranteed.
@@ -399,6 +399,7 @@ class EtcdChangeWatcher(EtcdWatcher):
                  wait_until_elected=False, recovery_time=5,
                  data=None, heartbeat=60):
         self.implemented_state = {}
+        self.watch_path = watch_path
 
         super(EtcdChangeWatcher, self).__init__(
             etcd_client, name, watch_path, election_path,
@@ -450,6 +451,34 @@ class EtcdChangeWatcher(EtcdWatcher):
         else:
             self.implemented_state[key] = value
 
-    @abstractmethod
     def key_change(self, action, key, value):
+        """Called when a key changes from the known value
+
+        This can be because it's added, changed, refreshed or deleted.
+        This default implementation does not notify of a change at
+        the root.  We assume only subkeys are interesting.
+        """
+
+        m = re.match(self.data.port_key_space + '^' + 
+                     re.escape(self.watch_path) + /([^/]+)$', key)
+        short_key = m.group(1)
+
+        if m:
+            if action == 'deleted':
+                self.removed(short_key)
+            else:
+                self.added(short_key, value)
+
+    def removed(self, key):
+        """Called when a key is deleted
+
+        The watch path is removed, leaving only the subpath.
+        """
+        pass
+
+    def added(self, key, value):
+        """Called when a key is added, changed, updated...
+
+        The watch path is removed, leaving only the subpath.
+        """
         pass
