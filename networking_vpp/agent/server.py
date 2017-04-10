267ce8ab8a2e0a6e4eff8839f942544cea16f104
Ian Wells <iawells@cisco.com>
2017-04-10 16:23:31 -0700

    Correct mac age setting
    VPP accepts mac ageing settings in minutes, and the config item is
    in seconds.  Perform a conversion.
    
    Change-Id: Id445c0b9ca1955286297f793e60ccff292bd361a

--- networking_vpp/agent/server.py
+++ networking_vpp/agent/server.py
@@ -1895,6 +1895,10 @@ def main():
                           "physnet2:<interface>"
                           )
                 sys.exit(1)
+
+    # Convert to the minutes unit that VPP uses:
+    # (we round *up*)
+    mac_age_min = (cfg.CONF.ml2_vpp.mac_age + 59) / 60
     vppf = VPPForwarder(physnets,
                         mac_age=cfg.CONF.ml2_vpp.mac_age,
                         tap_wait_time=cfg.CONF.ml2_vpp.tap_wait_time,
