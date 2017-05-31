679b96b45b3798d62343d59375fc860eb5eb88da
Naveen Joy <najoy@cisco.com>
2017-05-31 14:08:12 -0700

    Enable remote-ip-prefix value to be used when set
    This patch fixes the bug that ignored the remote-ip-prefix
    value when remote-group-id is set to None.
    
    Closes-Bug: #1694828
    Change-Id: If43f52c719bbfff0ecc3a110151ab762629e45c2

--- networking_vpp/mech_vpp.py
+++ networking_vpp/mech_vpp.py
@@ -766,10 +766,10 @@ class EtcdAgentCommunicator(AgentCommunicator):
         # VPP uses all zeros to represent any Ipv4/IpV6 address
         # TODO(najoy) handle remote_group_id when remote_ip_prefix is None
         if (rule['remote_ip_prefix'] is None
-                or rule['remote_group_id'] is None):
+                and rule['remote_group_id'] is None):
             remote_ip_addr = '0.0.0.0' if not is_ipv6 else '0:0:0:0:0:0:0:0'
             ip_prefix_len = 0
-        else:
+        elif rule['remote_ip_prefix']:
             remote_ip_addr, ip_prefix_len = rule['remote_ip_prefix'
                                                  ].split('/')
         # TODO(najoy): Add support for remote_group_id in sec-group-rules
@@ -793,7 +793,8 @@ class EtcdAgentCommunicator(AgentCommunicator):
         else:
             port_min, port_max = (rule['port_range_min'],
                                   rule['port_range_max'])
-        sg_rule = SecurityGroupRule(is_ipv6, remote_ip_addr, ip_prefix_len,
+        sg_rule = SecurityGroupRule(is_ipv6, remote_ip_addr,
+                                    int(ip_prefix_len),
                                     protocol, port_min, port_max)
         return sg_rule
 
