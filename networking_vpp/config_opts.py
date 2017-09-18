1d1fc23a54298bb5e17523a955164811ece87a14
jb <jbeuque@cisco.com>
2017-09-18 16:03:30 +0200

    Implementation of Tap as a Service
    Change-Id: Ibd28c936ba4f13e3822b495e43ad80781df81c1f

--- networking_vpp/config_opts.py
+++ networking_vpp/config_opts.py
@@ -83,6 +83,8 @@ _vpp_opts = [
     cfg.StrOpt('vpp_base_mac', default="fa:16:3c:00:00:00",
                help=_("The base MAC address used for VPP."
                       "The last 3 octets will be randomly generated.")),
+    cfg.StrOpt('agent_extensions', default='',
+               help=_("Enabled extensions for the VPP control system"))
 ]
 
 
