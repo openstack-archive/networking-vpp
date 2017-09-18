1df96d88d30fd67685303f96b443b15fa6011a18
jb <jbeuque@cisco.com>
2017-09-18 16:03:30 +0200

    Implementation of Tap as a Service
    Change-Id: Ibd28c936ba4f13e3822b495e43ad80781df81c1f

--- networking_vpp/mech_vpp.py
+++ networking_vpp/mech_vpp.py
@@ -42,6 +42,8 @@ from networking_vpp.compat import events
 from networking_vpp.compat import registry
 from networking_vpp.compat import resources
 
+from neutron.db import api as neutron_db_api
+
 
 try:
     # Newton and on
@@ -89,6 +91,10 @@ class VPPMechanismDriver(api.MechanismDriver):
         compat.register_securitygroups_opts(cfg.CONF)
 
         self.communicator = EtcdAgentCommunicator(self.port_bind_complete)
+        self.etcdJournalHelper = \
+            etcdutils.EtcdJournalHelper(
+                self.communicator,
+                neutron_db_api.get_writer_session())
 
     def get_vif_type(self, port_context):
         """Determine the type of the vif to be bound from port context"""
