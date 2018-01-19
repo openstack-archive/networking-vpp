7457fee013b996c9d501ab93f449e5d44567d62a
Ihar Hrachyshka <ihrachys@redhat.com>
2018-01-19 12:53:32 -0800

    DNM testing whether lib/neutron switch breaks this repo
    Depends-On: Iddbb60dd5fd1c723efda6ddb8c70006c809ed43a
    Change-Id: I1fbad166b5bd9b3b0f7862fce29d3aa27ff71478

--- networking_vpp/version.py
+++ networking_vpp/version.py
@@ -1,4 +1,4 @@
-#    Copyright 2011 OpenStack Foundation
+#    Copyright 2011 OpenStack Foundation 
 #    Copyright 2017 Cisco Systems, Inc.
 #
 #    Licensed under the Apache License, Version 2.0 (the "License"); you may
